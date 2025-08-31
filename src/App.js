fea32b4b21285304c7d7b905fe038d0d667a884c
Biswadip-saha <biswadip.learn@gmail.com>
2025-08-31 20:54:04 +0530

    calculating stats

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function App() {
 			<Logo />
 			<Form onAddItems={handleAddItems} />
 			<PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-			<Stats />
+			<Stats items={items} />
 		</div>
 	);
 }
@@ -85,10 +85,16 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 	);
 }
 
-function Stats() {
+function Stats({ items }) {
+	if(!items.length) return <p className="stats"><em>Start adding some items to your packing list 🚀</em></p>
+
+	const numItems = items.length;
+	const numPacked = items.filter((item) => item.packed).length;
+	const percentage = Math.round((numPacked / numItems) * 100);
+
 	return (
 		<footer className="stats">
-			<em>👜 You have X items on your list, and you already packed X (X%)</em>
+			<em>{percentage === 100 ? "You got everything! Ready to go ✈️" : `👜 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}</em>
 		</footer>
 	);
 }
